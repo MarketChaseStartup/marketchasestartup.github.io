44867b68266612e6fce17358aa22a30089dd84ff
Felipe Hanashiro <felipe.hanashiro@exla.com.br>
2015-03-12 21:43:54 -0300

    new header

--- scripts/controllers/header.js
+++ scripts/controllers/header.js
@@ -13,5 +13,36 @@ mkcApp.controller('CtrlHeader',['$scope','$location','$modal', function($scope,$
 		$location.url('/cadastro');
     }
 
+    $scope.Page = {
+    	isOnEntrance: function(){
+    		var loc = $location.url();
+    		if(loc === '/' || loc === '/cadastro'){
+				return true;
+    		}return false;
+    	}
+    }
+
+    $scope.Logo = {
+    	img : function(){
+    		if($scope.Page.isOnEntrance()){
+    			return 'images/mkc_logo.png';
+    		}return 'images/mkc_logo_icon.png';
+    	},
+    	url: function(){
+    		if($scope.Page.isOnEntrance()){
+    			$location.url('/');
+    		}else{
+				$location.url('/anuncios/lista')
+    		}
+    	},
+    	align: function(){
+    		if($scope.Page.isOnEntrance()){
+    			return 'logo-left';
+    		}else{
+				return 'logo-center';
+    		}
+    	}
+    }
+
 }]);
 
--- scripts/controllers/login.js
+++ scripts/controllers/login.js
@@ -1,14 +1,20 @@
-mkcApp.controller('CtrlLogin',['$scope', function($scope){
+mkcApp.controller('CtrlLogin',['$scope','$location', function($scope,$location){
     
     $scope.Login = {
         user: '',
         password: '',
         enter: function(){
     		if($scope.Login.testLogin()){
-
+                $location.url('anuncios/lista');
+                setTimeout(function(){
+                    $('.modal').click();
+                },100);
     		}
         },
         testLogin: function(){
+            if($scope.Login.user !== '' && $scope.Login.password !== ''){
+                return true;
+            }
         	Plugins.Mensagem.alerta("Login Inválido");
         	return false;
         }

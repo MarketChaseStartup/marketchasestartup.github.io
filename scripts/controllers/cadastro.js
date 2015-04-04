da01cda6a8a4071609318e944e383347a1fee607
Felipe Hanashiro <felipe.hanashiro@exla.com.br>
2015-04-03 23:01:37 -0300

    integration - enderecos

--- scripts/controllers/cadastro.js
+++ scripts/controllers/cadastro.js
@@ -1,26 +1,45 @@
-mkcApp.controller('CtrlCadastro',['$scope','FctLoja', function($scope,FctLoja){
+mkcApp.controller('CtrlCadastro',['$scope','FctLoja', 'FctDataSource','$timeout', function($scope,FctLoja, FctDataSource, $timeout){
     
     $scope.Loja = FctLoja;
     FctLoja.select(0);
 
+    $scope.listaCidades = [];
+
     $scope.estados = Colecao.Estados;
-    $scope.tipoEmail = Colecao.tiposEmail;
-    $scope.tipoTelefone = Colecao.tiposTelefone;
     $scope.tipoLogradouro = Colecao.tiposLogradouro;
     /*$scope.cidades = Utils.Moradia.retornarCidade($scope.Cliente.cliente.endereco.estado.Key);
     $scope.telefone = Objetos.Contato();
     $scope.email = Objetos.Contato();*/
 
     /******* FUNCOES *******/
-    $scope.retornarCidades = function(){
-        $scope.cidades = Utils.Moradia.retornarCidade($scope.Cliente.cliente.endereco.estado.Key);
-        $scope.Cliente.cliente.Endereco.Cidade = {};
+    $scope.retornarCidades = function(index, state){
+        $scope.listaCidades[index] = Utils.Moradia.retornarCidade((state || $scope.Loja.selected.obj.listaEnderecos[index].estado.Key));
+        if(!state){
+            $scope.Loja.selected.obj.listaEnderecos[index].cidade = "";
+        }
     }
 
 
     $scope.setEstadoCidade = function(){
         //Controle.setEstadoCidade($scope);
     }
+
+
+    var initCities = function(){
+        if($scope.Loja.selected.index === -1){
+            return false;
+        }
+        var cities = $scope.Loja.selected.obj.listaEnderecos.length;
+        while(cities--){
+            var state = Utils.Moradia.estadoKey($scope.Loja.selected.obj.listaEnderecos[cities].estado);
+            $scope.retornarCidades(cities, state);
+        }
+    }
+
+    $timeout(function(){
+        initCities();
+    },1000);
+    
 
 }]);
 
